78b3c5bc7f3974f3b34663adf4c14557ecc1b484
pixelcutter <nwmusicview@gmail.com>
2023-09-22 03:18:44 -0700

    added godotenv dep. Server now starts and listens to a port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type Config struct {
+	Port string
+}
+
+type Application struct {
+	Config Config
+}
+
+func (app *Application) Serve() error {
+	// port := os.Getenv("PORT")
+	fmt.Println("API is listening on port", app.Config.Port)
+
+	srv := &http.Server {
+		Addr: fmt.Sprintf(":%s", app.Config.Port),
+		// TODO: add router
+	}
+	return srv.ListenAndServe()
+}
+
 func main() {
-	fmt.Println("API is running...")
+	err := godotenv.Load()
+	if err != nil { 
+		log.Fatal("Error loading .env")
+	}
+
+	cfg := Config {
+		Port: os.Getenv("PORT"),
+	}
+
+	// TODO: connection to db
+
+	app := &Application{
+		Config: cfg,
+	}
+
+	err = app.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Pixelcutter/units_backend
+
+go 1.18
+
+require github.com/joho/godotenv v1.5.1
