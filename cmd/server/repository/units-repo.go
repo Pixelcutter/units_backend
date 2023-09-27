e45a311e15faa65f0cae34d1895f5262df1ee3e1
pixelcutter <nwmusicview@gmail.com>
2023-09-26 22:16:21 -0700

    starting to add repository code

---
+++ cmd/server/repository/units-repo.go
@@ -0,0 +1,21 @@
+package repository
+
+import "github.com/Pixelcutter/units_backend/cmd/server/model"
+
+type UnitsRepository interface {
+	SaveUser(model.User) (model.User, error)
+	FindAllUser() ([]model.User, error)
+	FindOneUser(int) (model.User, error)
+	UpdateUser(model.User) (model.User, error)
+	DeleteUser(int) error
+	SaveItem(model.Item) (model.Item, error)
+	FindAllItem() ([]model.Item, error)
+	FindOneItem(int) (model.Item, error)
+	UpdateItem(model.Item) (model.Item, error)
+	DeleteItem(int) error
+	SaveCategory()
+	FindAllCategories()
+	FindOneCategory()
+	UpdateCategory()
+	DeleteCategory()
+}
